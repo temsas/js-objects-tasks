66926f5dd9bda797ed5a6fee5723d1d090ebaf60
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:26:58 +0300

    Задание 3.5 выполнено

--- solutions/5-property-existence.js
+++ solutions/5-property-existence.js
@@ -1,5 +1,22 @@
 import _ from 'lodash';
 
 // BEGIN
+const countWords = (text) =>{
+    if (!text){
+        return {};
+    }
+    const words = _.words(text);
+    const wordCount = {};
 
+    for ( const word of words){
+        const lowerWords = word.toLowerCase();
+        if(wordCount[lowerWords]){
+            wordCount[lowerWords] +=1;
+        } else {
+            wordCount[lowerWords] = 1;
+        }
+    }
+return wordCount;
+}
+export default countWords;
 // END
