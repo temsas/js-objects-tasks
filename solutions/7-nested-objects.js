508225bd4779f9813a98af40e538cf5db9b772e8
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:27:19 +0300

    Задание 3.7 выполнено

--- solutions/7-nested-objects.js
+++ solutions/7-nested-objects.js
@@ -1,3 +1,16 @@
 // BEGIN
-
+const get = (object, keys) =>{
+    if (!object || !keys) {
+        return null;
+    }
+    let objectDash = object;
+    for ( const key of keys){
+        if(!Object.hasOwn(objectDash, key) ){
+            return null;
+        }
+            objectDash = objectDash[key]
+    }
+    return objectDash;
+}
+export default get;
 // END
