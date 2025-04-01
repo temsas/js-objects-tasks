3d00f602626ab1b3524cf3e457d7c1c214b5723a
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:27:36 +0300

    Задание 3.10 выполнено

--- solutions/10-spread-operator.js
+++ solutions/10-spread-operator.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+const make = (name, obj) =>{
+    const defaultObj = {
+        state: 'moderating',
+        createdAt: Date.now(),
+    }
+    return {
+        name,
+        ...defaultObj,
+        ...obj,
+    }
+}
+export default make;
 // END
