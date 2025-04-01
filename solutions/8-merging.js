8722991587037476b011fafe2334988698404384
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:27:26 +0300

    Задание 3.8 выполнено

--- solutions/8-merging.js
+++ solutions/8-merging.js
@@ -1,5 +1,14 @@
 import _ from 'lodash';
 
 // BEGIN
-
+const fill = (company, keys, data) =>{
+    if(keys.length > 0){
+    const pickData = _.pick(data, keys);
+    Object.assign(company, pickData);
+    } else {
+        Object.assign(company, data);
+    }
+return company;
+}
+export default fill;
 // END
