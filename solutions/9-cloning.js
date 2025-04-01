eecac397010dc2626ed7fb9d946e02566681af27
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:27:30 +0300

    Задание 3. выполнено

--- solutions/9-cloning.js
+++ solutions/9-cloning.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const cloneShallow = (obj) =>{
+    let cloneObject = {};
+    for(const key in obj){
+        if(Object.hasOwn(obj, key)){
+            cloneObject[key] = obj[key];
+        }
+    }
+    return cloneObject;
+}
+export default cloneShallow;
 // END
