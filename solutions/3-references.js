949eaa28cd018279a2a705517627840656d3e23c
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:26:47 +0300

    Задание 3.3 выполнено

--- solutions/3-references.js
+++ solutions/3-references.js
@@ -1,3 +1,9 @@
 // BEGIN
-
+const boolCompany = (c1, c2) =>{
+    if(c1.website === c2.website &&  c1.state === c2.state && c1.name === c2.name) {
+        return true
+    } else
+        return false
+}
+export default boolCompany;
 // END
