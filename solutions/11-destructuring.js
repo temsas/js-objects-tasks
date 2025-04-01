066c9d362573e2c6a8768f48d9aa2c4196d6fc5e
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:27:42 +0300

    Задание 3.11 выполнено

--- solutions/11-destructuring.js
+++ solutions/11-destructuring.js
@@ -1,3 +1,11 @@
 // BEGIN
+const getSortedNames = (users) =>{
+    const names = [];
 
+    for(const {name} of users){
+        names.push(name)
+    }
+    return names.sort();
+}
+export default getSortedNames;
 // END
