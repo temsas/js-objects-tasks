55054eb4316fcfcc5676d69eb82fbb2b4e24eb63
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:26:42 +0300

    Задание 3.2 выполнено

--- solutions/2-modifications.js
+++ solutions/2-modifications.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
 
 // BEGIN
+const normalize=(lesson)=>{
+    lesson.name =_.capitalize(lesson.name);
+    lesson.description = lesson.description.toLowerCase();
+    return lesson;
+}
+export default normalize;
 
-  // END
+// END
