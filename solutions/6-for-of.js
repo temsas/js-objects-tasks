9b398dd832eeacc1a6dda9aec1e5a106543c02ba
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:27:03 +0300

    Задание 3.6 выполнено

--- solutions/6-for-of.js
+++ solutions/6-for-of.js
@@ -1,3 +1,15 @@
 // BEGIN
-
+const pick = (object, keys) =>{
+    const result = {};
+    if(!object || !keys){
+        return result;
+    }
+    for( const key of keys){
+        if(Object.hasOwn(object, key)){
+            result [ key] = object[key];
+        }
+    }
+    return result;
+}
+export default pick;
 // END
