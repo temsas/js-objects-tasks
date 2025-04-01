dbe1148cb6eac7030e82b231f1a8dcd45a1fb9dc
temsas <artemkariakin1187@gmail.com>
2025-04-01 15:26:52 +0300

    Задание 3.4 выполнено

--- solutions/4-using.js
+++ solutions/4-using.js
@@ -1,3 +1,18 @@
 // BEGIN
+const getDomainInfo = (domen) =>{
+    let name = domen;
+    let header = 'http'
 
+    if(domen.startsWith('https://')){
+        header = 'https';
+        name = domen.replace("https://", '');
+    }else if (domen.startsWith('http://')){
+        name = domen.replace("http://", '')
+    }
+    return {
+        scheme: header,
+        name: name,
+    };
+}
+export default getDomainInfo;
 // END
